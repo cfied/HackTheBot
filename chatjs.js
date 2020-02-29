871debbfe5537b12187789746432dbf2b892db94
aamaropatino <aamaropatino@users.noreply.github.com>
2020-02-29 14:59:42 +0000

    b

--- chatjs.js
+++ chatjs.js
@@ -1,6 +1,5 @@
 (function () {
     var Message;
-    alert(12);
     Message = function (arg) {
         this.text = arg.text, this.message_side = arg.message_side;
         this.draw = function (_this) {
