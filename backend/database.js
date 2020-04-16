954c780f388d86f5f34abc9cbc51ab0cd989644b
Carina <carina.x.fiedler@gmail.com>
2020-04-16 13:32:52 +0100

    get rid of rooms, send messages to assigned partner socket

--- backend/database.js
+++ backend/database.js
@@ -13,28 +13,28 @@ const pool = mysql.createPool({
 pool.pool.config.connectionConfig.namedPlaceholders = true;
 
 // user table format:
-// "CREATE TABLE users (user_id VARCHAR(255), room_id INT, PRIMARY KEY(user_id));"
+// "CREATE TABLE users (user_id VARCHAR(255), partner VARCHAR(255), PRIMARY KEY(user_id));"
 
 // message table format:
-// "CREATE TABLE messages (message_id int NOT NULL AUTO_INCREMENT, user_id VARCHAR(255) NOT NULL, room_id INT NOT NULL, content VARCHAR(511) NOT NULL, PRIMARY KEY(message_id));"
+// "CREATE TABLE messages (message_id int NOT NULL AUTO_INCREMENT, user_id VARCHAR(255) NOT NULL, content VARCHAR(511) NOT NULL, PRIMARY KEY(message_id));"
 
 
-async function add_message(user_id, room_id, message_content){
-  sql = "INSERT INTO messages (user_id,room_id,content) VALUES (?,?,?);";
-  await pool.execute(sql, [user_id, room_id, message_content]);
+async function add_message(user_id, message_content){
+  sql = "INSERT INTO messages (user_id,content) VALUES (?,?);";
+  await pool.execute(sql, [user_id, message_content]);
 }
 
-async function get_messages(room_id){
-  sql = "SELECT * FROM messages WHERE room_id = ?;";
-  const [rows, fields] = await pool.execute(sql,[room_id]);
+async function get_messages(partner){
+  sql = "SELECT * FROM messages WHERE user_id = ?;";
+  const [rows, fields] = await pool.execute(sql,[partner]);
   return rows;
 }
 
-async function delete_room(room_id){
-  sql = "DELETE FROM messages WHERE room_id = ?;";
-  await pool.execute(sql, [room_id]);
+async function delete_messages(user1, user2){
+  sql = "DELETE FROM messages WHERE user_id = ? OR user_id = ?;";
+  await pool.execute(sql, [user1, user2]);
 }
 
 module.exports.add_message = add_message;
 module.exports.get_messages = get_messages;
-module.exports.delete_room = delete_room;
+module.exports.delete_messages = delete_messages;
--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 // 0 : bot or 1: human
 var mode = {}
 var available_rooms = [];
-var i = -1;
+var matched = {};
 
 // returns room number and assigns partners if possible
+// room -1 : chatbot room -2 : awaiting connection
 function assign_room_number(id){
-  console.log("available " + available_rooms );
   rand = Math.random();
   if(rand < 0.5){
     mode[id] = 0;
@@ -26,47 +26,48 @@ function assign_room_number(id){
   }
 
   if(mode[id] == 0){
-    return -1;
+    matched[id] = -1
   }else{
     if(available_rooms.length != 0){
-      room = available_rooms.pop();
-      console.log("asigning " + room);
-      return room[0];
+      partner = available_rooms.pop();
+      matched[id] = partner;
+      matched[partner] = id;
     }else{
-      i++;
-      available_rooms.push([i, id]);
-      console.log("available " + available_rooms);
-      return i;
+      available_rooms.push(id);
+      matched[id] = -2;
     }
   }
 }
 
 // when user connects, assign a room number and display messages
 io.on('connection', function(socket){
-  var room;
+  var partner;
   console.log('user connected');
 
   socket.on('request room number', () => {
-    room = assign_room_number(socket.id);
-    console.log("assigned " + socket.id + " to room "+ room);
-    if(room != -1){
-      socket.join(room);
+    assign_room_number(socket.id);
+    partner = matched[socket.id];
+    console.log("matched " + socket.id + " to "+ partner);
+    if(partner != -1 && partner != -2){
+      database.get_messages(partner).then(rows => {
+        console.log(rows[0]);
+        for(index in rows){
+          console.log(rows[index].content);
+          socket.emit('chat message', rows[index].content);
+        }
+      });
     }
-    database.get_messages(room).then(rows => {
-      console.log(rows[0]);
-      for(index in rows){
-        console.log(rows[index].content);
-        socket.emit('chat message', rows[index].content);
-      }
-    });
   });
 
   // received chat message from user
   socket.on('chat message', function(msg){
-    database.add_message(socket.id,room,msg);
+    partner = matched[socket.id];
+    database.add_message(socket.id,msg);
     // send message to other user in human mode
     if(mode[socket.id] == 1){
-  		socket.to(room).broadcast.emit('chat message', msg);
+      if(partner != -2){
+        socket.to(partner).emit('chat message', msg);
+      }
     // or send bot response
     }else{
       response = bot.respond(msg)
@@ -77,16 +78,19 @@ io.on('connection', function(socket){
   });
   // user took a guess
   socket.on('check mode', function(val){
+    partner = matched[socket.id];
     if(Number(val) == mode[socket.id]){
       bool = 1;
     }else{
       bool = 0;
     }
     if(mode[socket.id] == 1){
-      if(bool == 1){
-        socket.to(room).broadcast.emit('decision', 'Oh no. Your opponent found out that you are a human.');
-      }else{
-        socket.to(room).broadcast.emit('decision', 'Congratulations! You tricked your opponent into thinking that you are a bot.');
+      if(partner != -2){
+        if(bool == 1){
+          socket.to(partner).emit('decision', 'Oh no. Your opponent found out that you are a human.');
+        }else{
+          socket.to(partner).emit('decision', 'Congratulations! You tricked your opponent into thinking that you are a bot.');
+        }
       }
     }
     socket.emit('mode bool', bool.toString());
@@ -94,10 +98,11 @@ io.on('connection', function(socket){
   });
   // user confirmed result
   socket.on('leave',() => {
-    socket.leave(room);
-    database.delete_room(room);
-    if(available_rooms.includes(room)){
-      available_rooms.remove(room);
+    partner = matched[socket.id];
+    database.delete_messages(socket.id, String(partner));
+    delete(matched[socket.id]);
+    if(available_rooms.includes(socket.id)){
+      available_rooms.splice(available_rooms.indexOf(socket.id),1);
     }
   });
 });
@@ -146,7 +151,6 @@ app.get('/jpg_background', function (req, res) {
 
 // starting chat
 app.get('/chat', function (req, res) {
-   console.log("Got a GET request for the homepage");
    fs.readFile('../html/chat.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
